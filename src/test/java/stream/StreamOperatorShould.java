f884c1817bdd0f4388ae04e82e76193e0c69753e
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-05-12 16:09:05 +0200

    map

--- src/test/java/stream/StreamOperatorShould.java
+++ src/test/java/stream/StreamOperatorShould.java
@@ -2,10 +2,12 @@ package stream;
 
 import football.player.Player;
 import helper.PlayerTestHelper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static java.util.stream.Collectors.toList;
@@ -13,10 +15,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StreamOperatorShould {
 
+    private List<Player> players;
+
+    @BeforeEach
+    void setUp() {
+        players = new PlayerTestHelper().getPlayers();
+    }
+
     @Test
     void filter_data() {
-        final List<Player> players = new PlayerTestHelper().getPlayers();
-
         Predicate<? super Player> topScorer = player -> player.getGoal() > 100;
         Predicate<? super Player> nameIsAli = player -> player.getName().contains("Ali");
         final List<Player> topScorerWhereNameContainsAli = players.stream()
@@ -27,5 +34,20 @@ public class StreamOperatorShould {
         final List<Player> aliDaei = new LinkedList<>();
         aliDaei.add(new Player("Ali Daei", 109));
         assertThat(topScorerWhereNameContainsAli).isEqualTo(aliDaei);
+    }
+
+    @Test
+    void map_data() {
+        Function<? super Player, ?> function = Player::getName;
+        final List<?> playerNames = players.stream()
+                .map(function)
+                .collect(toList());
+
+        final List<String> expectedResult = new LinkedList<>();
+        expectedResult.add("Ali Daei");
+        expectedResult.add("Cristiano Ronaldo");
+        expectedResult.add("Ferenc Puskás");
+        expectedResult.add("Mokhtar Dahari");
+        assertThat(playerNames).isEqualTo(expectedResult);
     }
 }
